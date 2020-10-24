fc7a15b75eece4f98db2223af467d4c6c9dea09a
dr-spaceman <mat.berti@gmail.com>
2020-10-24 23:12:43 +0800

    Colophon: change message and remove Link from diorama

--- browser/src/components/Colophon.jsx
+++ browser/src/components/Colophon.jsx
@@ -8,7 +8,7 @@ export default function Colophon() {
         return '';
     }
     // Prevent saving hidden status:
-    // localStorage.clear();
+    localStorage.clear();
 
     const [open, setOpen] = React.useState(true);
 
@@ -20,13 +20,13 @@ export default function Colophon() {
 
     if (open && localStorage.getItem('colophon') !== 'closed') {
         return (
-            <div className="container dark" style={{ position: 'fixed', zIndex: 999, right: 0, bottom: 0, left: 0, fontSize: '15px', color: '#BBB', boxShadow: '0 0 10px -5px black' }}>
-                <div style={{ padding: '30px 0', margin: '0 auto', textAlign: 'center' }}>
-                    Welcome to Start Game Options, a site about videogames.
+            <div className="container dark type-retro" style={{ position: 'fixed', zIndex: 999, right: 0, bottom: 0, left: 0, color: '#BBB' }}>
+                <div style={{ padding: '50px 0 40px 0', margin: '0 auto', textAlign: 'center' }}>
+                    Start Game is the secret
                     <br />
-                    <a href="/about.php"><strong>Read more</strong></a> about this site or else <a href="#close" title="hide this message and don't show it to me again" className="tooltip" onClick={handleClose}><strong>pay me for the door repair charge</strong></a>.
+                    <a href="#close" title="hide this message and don't show it to me again" className="tooltip" onClick={handleClose}>Now Pay me for the door repair charge</a>
                 </div>
-                <div style={{ position: 'absolute', zIndex: 2, top:'10px', left: '50%', width:'192px', height:'16px', margin: '0 0 0 -96px', background: `url(${welcome}) no-repeat 0 0` }} />
+                <div style={{ position: 'absolute', zIndex: 2, top:'20px', left: '50%', width:'192px', height:'16px', margin: '0 0 0 -96px', background: `url(${welcome}) no-repeat 0 0` }} />
                 <div style={{ position: 'absolute', zIndex: 2, bottom:'0', left: '50%', width:'192px', height:'18px', margin: '0 0 0 -96px', background: `url(${welcome}) no-repeat 0 -16px` }} />
                 <div style={{ position: 'absolute', zIndex: 1, right:'0', bottom: '0', left:'0', width:'100%', height: '18px', background: `url(${welcome}) repeat-x 0 -34px` }} />
             </div>
